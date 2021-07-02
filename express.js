9d77170b9e8a5a4594324f72336824421e868b70
DenisaRosu <rosu_rebeca_denisa@yahoo.com>
2021-07-02 16:52:38 +0300

    Last commit

--- express.js
+++ express.js
@@ -1,11 +1,11 @@
 var express = require('express')
 var app = express()
 //default endpoind s
-app.get('/',(req,res) => {
+app.get('./summer',(req,res) => {
   res.send('welocme')
 })
 
-app.get('stud',())
+//app.get('stud',(){)
 
 app.listen(3000,() => {
     console.log("Server is work")
