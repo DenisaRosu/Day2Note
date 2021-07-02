b751775b401ed968cfdd43620adafdfa3802fce7
DenisaRosu <rosu_rebeca_denisa@yahoo.com>
2021-07-02 16:44:15 +0300

    Secound modification

--- ourmodule.js
+++ ourmodule.js
@@ -1,3 +1,9 @@
-exports.myDateTime = function () {
+exports.ourFunction = function () {
     return Date();
-  };
+  }
+exports.PlusFunction = function (a,b) {
+    return a+b  ;
+  }
+exports.MinusFunction= function (a,b){
+  return b-a; 
+}
